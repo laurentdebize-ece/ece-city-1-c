5487e8bf66fe4d9e32b299a43ada5bcd54164eab
MasaoCoudeville <masao.coudeville@edu.ece.fr>
2022-10-25 18:33:42 +0200

    gitignore finito

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include "main.h"
 
 int main(){
-    printf("test");
-    printf("MASAOOOOOO");
-    printf("tiiiiii");
-    printf("hello");
+    printf("Hehe");
     return 0;
-    printf("hello");
 }
